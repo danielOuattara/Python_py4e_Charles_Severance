74f667e07f8f5ccfa10e41f9fbb996f171c0de52
danielOuattara <daniel.ouattara@gmx.com>
2022-11-02 00:15:36 +0100

    08 python  files

---
+++ 08_Python_Files/autograder_files.py
@@ -0,0 +1,13 @@
+# Use the file name mbox-short.txt as the file name
+fname = input("Enter file name: ")
+count = 0
+total = 0
+fh = open(fname)
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:"):
+        continue
+    new_line = line.split()
+    total += float(new_line[1])
+    count += 1
+
+print("Average spam confidence:", total / count)
---
+++ 08_Python_Files/script_2.py
@@ -0,0 +1,98 @@
+print(70 * "-")
+
+# File handle as a Sequence
+file_handle = open("demo.txt")
+for line in file_handle:
+    print(line)
+
+print(70 * "-")
+
+# Counting Number Of Lines In a File
+file_handle = open("demo.txt")
+count = 0
+for line in file_handle:
+    count += 1
+
+print("Line count : ", count)
+
+print(70 * "-")
+
+# Reading the Whole File
+file = open("demo.txt")
+file_itself = file.read()
+print(len(file_itself))
+print(file_itself)
+
+print(70 * "-")
+
+print(file_itself[: 140])
+
+print(70 * "-")
+
+# Searching Through a File
+file = open("demo.txt")
+for line in file:
+    if line.startswith("Line"):
+        print(line)
+
+print(70 * "-")
+
+# Searching Through a File, remove newline if any
+file = open("demo.txt")
+for line in file:
+    line = line.rstrip()
+    if line.startswith("Line"):
+        print(line)
+
+print(70 * "-")
+
+# Searching Through a File, remove newline if any + Skipping not matching with Continue keyword
+file = open("demo.txt")
+for line in file:
+    line = line.rstrip()
+    if not line.startswith("Line"):
+        continue
+    print(line)
+
+print(70 * "-")
+
+# Searching Through a File, remove newline if any + Skipping not matching with "continue" keyword + "in" operator
+file = open("demo.txt")
+for line in file:
+    line = line.rstrip()
+    if "Line" not in line:
+        continue
+    print(line)
+
+print(70 * "-")
+
+# Prompt for File Name
+file_name = input("Enter the file name: ")
+print("-" * 3)
+count = 0
+file = open(file_name)
+for line in file:
+    if "Line" in line:
+        count += 1
+
+print("count = ", count)
+
+print(70 * "-")
+
+#  check For Prompted for File Name
+file_name = input("Enter the file name: ")
+print("-" * 3)
+try:
+    file = open(file_name)
+except:
+    print("File Not Found", file_name)
+    quit()
+
+count = 0
+for line in file:
+    if "Line" in line:
+        count += 1
+
+print("count = ", count)
+
+print(70 * "-")
