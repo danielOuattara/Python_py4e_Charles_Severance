8669faa8f9658c0faebea36d731db52bb7478ba3
danielOuattara <daniel.ouattara@gmx.com>
2022-11-02 00:14:50 +0100

    05 function

---
+++ 05_Python_Function/autograder_1.py
@@ -0,0 +1,17 @@
+def compute_pay(hours, rate):
+    hours = float(input('Enter yours worked hours : '))
+    rate = float(input('Enter yours rate pay : '))
+    hours_rate = 40
+
+    if hours > hours_rate:
+        basic_pay = hours_rate * rate
+        regulated_rate = rate * 1.5
+        pay_amount = basic_pay + (hours - hours_rate) * regulated_rate
+    else:
+        pay_amount = hours * rate
+
+    return pay_amount
+
+
+p = compute_pay(10, 20)
+print("Pay", p)
