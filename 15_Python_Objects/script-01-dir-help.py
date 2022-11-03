f4b8275971ffc3229cbf95578fa20f179cac53f3
danielOuattara <daniel.ouattara@gmx.com>
2022-11-03 09:52:45 +0100

    python objects

---
+++ 15_Python_Objects/script-01-dir-help.py
@@ -0,0 +1,30 @@
+x = 'abc'
+print(type(x))
+
+print(type(2.5))
+
+print(type(2))
+
+y = list()
+print(type(y))
+
+z = dict()
+print(type(z))
+
+print('\n')
+# ====================================
+
+print(dir(x))
+print('\n')
+
+print(dir(y))
+print('\n')
+
+print(dir(z))
+print('\n' * 5)
+
+# ====================================
+
+print('\n' * 7, help(x))
+print('\n' * 7, help(y))
+print('\n' * 7, help(z))
