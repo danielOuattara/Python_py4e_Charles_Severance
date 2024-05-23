b23a3244a70660bae8a8d2bee32e8690796f05c3
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-05-23 10:08:12 +0200

    08 files

---
+++ 08_Python_Files/01_file_processing.py
@@ -0,0 +1,31 @@
+# File processing: mostly text file here
+
+# Opening a File
+# --------------------
+
+#  open() to handle a file: open , read, write, close, mostly in utf-8
+# -------------------------------------------------
+# file_handle = open("filename", "mode")
+
+# The newline Character: "\n", it's count for 1 character
+stuff = "Hello \nWorld of Python!"              
+print(stuff)
+print(len(stuff))
+
+# What is 'handle'
+file_handle = open("demo.txt", "r")
+print(file_handle)
+file_handle.close()
+
+#  When the file is missing: --> Traceback
+
+# File processing
+
+
+
+
+
+
+
+
+
--- 08_Python_Files/02_reading_file.py
+++ 08_Python_Files/02_reading_file.py
@@ -1,27 +1,31 @@
 print(70 * "-")
 
-# File handle as a Sequence
+# ----- File handle as a Sequence
 file_handle = open("demo.txt")
 for line in file_handle:
     print(line)
 
+file_handle.close()
 print(70 * "-")
 
-# Counting Number Of Lines In a File
+# ----- Counting Number Of Lines In a File
 file_handle = open("demo.txt")
 count = 0
 for line in file_handle:
     count += 1
-
 print("Line count : ", count)
+
+file_handle.close()
 
 print(70 * "-")
 
-# Reading the Whole File
+# ------ Reading the Whole File
 file = open("demo.txt")
 file_itself = file.read()
 print(len(file_itself))
 print(file_itself)
+
+file_handle.close()
 
 print(70 * "-")
 
@@ -29,34 +33,44 @@ print(file_itself[: 140])
 
 print(70 * "-")
 
-# Searching Through a File
+# ----- Searching Through a File
 file = open("demo.txt")
 for line in file:
     if line.startswith("Line"):
         print(line)
 
+file_handle.close()
+
 print(70 * "-")
 
-# Searching Through a File, remove newline if any
+# ----- Searching Through a File, remove newline if any
 file = open("demo.txt")
 for line in file:
-    line = line.rstrip()
     if line.startswith("Line"):
+        line = line.rstrip()
         print(line)
 
+file_handle.close()
+
 print(70 * "-")
 
-# Searching Through a File, remove newline if any + Skipping not matching with Continue keyword
+# ----- Searching Through a File, remove newline if any + Skipping
+#       not matching with Continue keyword
 file = open("demo.txt")
 for line in file:
-    line = line.rstrip()
     if not line.startswith("Line"):
         continue
+    line = line.rstrip()
     print(line)
+
+file_handle.close()
 
 print(70 * "-")
 
-# Searching Through a File, remove newline if any + Skipping not matching with "continue" keyword + "in" operator
+# ----- Searching Through a File, remove newline if
+#       any + Skipping not matching with "continue"
+#       keyword + "in" operator
+
 file = open("demo.txt")
 for line in file:
     line = line.rstrip()
@@ -64,9 +78,11 @@ for line in file:
         continue
     print(line)
 
+file_handle.close()
+
 print(70 * "-")
 
-# Prompt for File Name
+# ----- Prompt for File Name
 file_name = input("Enter the file name: ")
 print("-" * 3)
 count = 0
@@ -77,9 +93,11 @@ for line in file:
 
 print("count = ", count)
 
+file_handle.close()
+
 print(70 * "-")
 
-#  check For Prompted for File Name
+# ------ check For Prompted for File Name
 file_name = input("Enter the file name: ")
 print("-" * 3)
 try:
@@ -92,6 +110,8 @@ count = 0
 for line in file:
     if "Line" in line:
         count += 1
+
+file.close()
 
 print("count = ", count)
 
