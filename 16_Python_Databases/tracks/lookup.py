8a491a0c027b7b74a2c82b2d8d571e8fa272d330
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-07-18 22:08:03 +0200

    16 Python Database:  deleted files & folders

--- 16_Python_Databases/tracks/lookup.py
+++
@@ -1,8 +0,0 @@
-def lookup(d, text):
-    found = False
-    for child in d:
-        if found:
-            return child.text
-        if child.tag == 'key' and child.text == text:
-            found = True
-    return None
