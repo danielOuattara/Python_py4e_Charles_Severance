12d77369832bb12af2fe12d83dfaf356ff90ce73
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-07-18 22:05:06 +0200

    16 Python Database: 04 many to many

---
+++ 16_Python_Databases/04_many_to_many/course-user-constraints.sql
@@ -0,0 +1,106 @@
+-- Python for Everybody Database Handout
+-- *
+-- https://www.py4e.com/lectures3/Pythonlearn-15-Database-Handout.txt
+-- *
+-- Download and Install: http://sqlitebrowser.org/
+-- NOTE: constraints are required in SQLite whereas they are not in DBMS like MySQL, PostgreSQL, etc.
+-- *
+-- Many-Many Relationship
+-- *
+CREATE TABLE
+    User(
+        "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+        "name" TEXT UNIQUE,
+        "email" TEXT
+    );
+
+CREATE TABLE
+    Course ("id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, "title" TEXT UNIQUE);
+
+CREATE TABLE
+    Member (
+        "user_id" INTEGER,
+        "course_id" INTEGER,
+        "role" INTEGER,
+        PRIMARY KEY (user_id, course_id),
+        FOREIGN KEY (user_id) REFERENCES User(id),
+        FOREIGN KEY (course_id) REFERENCES Course (id)
+    );
+
+INSERT INTO
+    User(name, email)
+VALUES
+    ('Jane', 'jane@tsugi.org');
+
+INSERT INTO
+    User(name, email)
+VALUES
+    ('Ed', 'ed@tsugi.org');
+
+INSERT INTO
+    User(name, email)
+VALUES
+    ('Sue', 'sue@tsugi.org');
+
+INSERT INTO
+    Course (title)
+VALUES
+    ('Python');
+
+INSERT INTO
+    Course (title)
+VALUES
+    ('SQL');
+
+INSERT INTO
+    Course (title)
+VALUES
+    ('PHP');
+
+INSERT INTO
+    Member (user_id, course_id, role)
+VALUES
+    (1, 1, 1);
+
+INSERT INTO
+    Member (user_id, course_id, role)
+VALUES
+    (2, 1, 0);
+
+INSERT INTO
+    Member (user_id, course_id, role)
+VALUES
+    (3, 1, 0);
+
+INSERT INTO
+    Member (user_id, course_id, role)
+VALUES
+    (1, 2, 0);
+
+INSERT INTO
+    Member (user_id, course_id, role)
+VALUES
+    (2, 2, 1);
+
+INSERT INTO
+    Member (user_id, course_id, role)
+VALUES
+    (2, 3, 1);
+
+INSERT INTO
+    Member (user_id, course_id, role)
+VALUES
+    (3, 3, 0);
+
+SELECT
+    User.name,
+    Member.role,
+    Course.title
+FROM
+    User
+    JOIN Member ON Member.user_id = User.id
+    JOIN Course ON Member.course_id = Course.id
+ORDER BY
+    Course.title,
+    Member.role DESC,
+    User.name;
